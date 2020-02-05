1814f378abbe019eb7eb69a55e6cd0941c0105ea
Henrique Eyer <henriqueeyer@outlook.com>
2020-02-05 10:24:01 -0300

    CC

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import LinkLogin from './LinkLogin';
 import BotaoSearch from './BotaoSearch';
 import SearchBar from './SearchBar';
