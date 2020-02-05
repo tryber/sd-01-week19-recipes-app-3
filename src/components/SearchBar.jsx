4d90a8bafb680526ab6ad6725657ebee90774e95
Henrique Eyer <henriqueeyer@outlook.com>
2020-02-05 14:26:14 -0300

    Preparando o merge com o header

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { DebounceInput } from 'react-debounce-input';
 
 
 const renderRadioButton = (value, changeSearch) => (
@@ -6,37 +7,48 @@ const renderRadioButton = (value, changeSearch) => (
     <input
       data-testid="ingredient-search-radio"
       type="radio"
-      checked={value === }
+      checked={value === 'i'}
       name="typeSearch"
-      value="Maior que"
+      value="i"
       onClick={(e) => changeSearch(e.target.value)}
     />
     Ingrediente
     <input
       data-testid="name-search-radio"
       type="radio"
-      checked={value === }
+      checked={value === 's'}
       name="typeSearch"
-      value="Menor que"
+      value="s"
       onClick={(e) => changeSearch(e.target.value)}
     />
     Nome
     <input
       data-testid="first-letter-search-radio"
       type="radio"
-      checked={value === }
+      checked={value === 'f'}
       name="typeSearch"
-      value="Igual a"
+      value="f"
       onClick={(e) => changeSearch(e.target.value)}
     />
     Primeira letra
     </div>
 );
 
-const SearchBar = () => (
-  <div>
-    <input type="text" />
-  </div>
-);
+const SearchBar = () => {
+  const [search, setSearch] = useState('');
+  const [typeSearch, setTypeSearch] = useState('i');
+  useEffect(() => {
+  }, [search])
+
+  return (
+    <div>
+      <DebounceInput
+        debounceTimeout={600}
+        onChange={event => setSearch(event.target.value)}
+      />
+      {renderRadioButton(typeSearch, setTypeSearch)}
+    </div>
+  )
+};
 
 export default SearchBar;
