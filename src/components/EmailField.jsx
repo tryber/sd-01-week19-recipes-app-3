564fd4094fa8baf7ca9015dad8eed75e6fff72e3
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-06 18:11:15 -0200

    Now is ok?

--- src/components/EmailField.jsx
+++ src/components/EmailField.jsx
@@ -11,25 +11,24 @@ const handleChange = (FiedlValue, setEmail, setIsEmailFormatted) => {
   }
 };
 
-const EmailField = ({ setEmail, setEmailValid, emailValid }) => {
-  return (
-    <Form.Group controlId="formBasicEmail">
-      <Form.Control
-        type="email"
-        placeholder="Email"
-        required="required"
-        data-testid="email-input"
-        onChange={(e) => handleChange(e.target.value, setEmail, setEmailValid)}
-        isValid={emailValid}
-        isInvalid={!emailValid}
-      />
-      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-      <Form.Control.Feedback type="invalid">
-        Enter a valid email address
+const EmailField = ({ setEmail, setEmailValid, emailValid }) => (
+  <Form.Group controlId="formBasicEmail">
+    <Form.Control
+      type="email"
+      placeholder="Email"
+      required="required"
+      data-testid="email-input"
+      onChange={(e) => handleChange(e.target.value, setEmail, setEmailValid)}
+      isValid={emailValid}
+      isInvalid={!emailValid}
+    />
+    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+    <Form.Control.Feedback type="invalid">
+      Enter a valid email address
       </Form.Control.Feedback>
-    </Form.Group>
-  );
-};
+  </Form.Group>
+);
+
 
 EmailField.propTypes = {
   setEmail: PropTypes.func.isRequired,
