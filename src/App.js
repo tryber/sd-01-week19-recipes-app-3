9dd4e56a34ff2cb1e36ed87aa7b3cf0bc82c99de
Henrique Eyer <henriqueeyer@outlook.com>
2020-02-06 17:17:55 -0300

    Terminando CC

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Foods from './components/Foods';
 import Drinks from './components/Drinks';
 import Home from './components/Home';
+// import CardRecipe from './components/CardRecipe';
 
 function App() {
   return (
     // <MenuInferior />
+    
     <Router>
+      {/* <CardRecipe /> */}
       <Switch>
-        <Route exact path="/" component={} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/comidas" component={Foods} />
         <Route exact path="/bebidas" component={Drinks} />
       </Switch>
