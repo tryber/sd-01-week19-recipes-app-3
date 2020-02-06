00f70429cbd6b24ad20906673fddc411945fae16
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-06 18:21:40 -0200

    button props

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
 
 const SubmitButton = ({ emailValid, passwordValid }) => {
   const fieldsValid = emailValid && passwordValid;
@@ -12,6 +13,16 @@ const SubmitButton = ({ emailValid, passwordValid }) => {
       variant={!fieldsValid ? 'outline-danger' : 'outline-success'}
     > Entrar </Button>
   );
+};
+
+SubmitButton.propTypes = {
+  emailValid: PropTypes.string,
+  passwordValid: PropTypes.string,
+};
+
+SubmitButton.defaultProps = {
+  emailValid: '',
+  passwordValid: '',
 };
 
 export default SubmitButton;
