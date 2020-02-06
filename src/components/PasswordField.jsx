34d2f514b76484b3ef6afdbd59765d08af688b00
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-06 18:09:12 -0200

    return direct

--- src/components/PasswordField.jsx
+++ src/components/PasswordField.jsx
@@ -11,26 +11,24 @@ const handleChange = (FiedlValue, setPassword, setIsPasswordFormatted) => {
   }
 };
 
-const PasswordField = ({ setPassword, setPasswordValid, passwordValid }) => {
-  return (
-    <Form.Group controlId="formBasicEmail">
-      <Form.Control
-        type="password"
-        placeholder="Senha"
-        required="required"
-        name="password"
-        data-testid="password-input"
-        onChange={(e) => handleChange(e.target.value, setPassword, setPasswordValid)}
-        isValid={passwordValid}
-        isInvalid={!passwordValid}
-      />
-      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-      <Form.Control.Feedback type="invalid">
-        Your password must contain at least 6 characters
+const PasswordField = ({ setPassword, setPasswordValid, passwordValid }) => (
+  <Form.Group controlId="formBasicEmail">
+    <Form.Control
+      type="password"
+      placeholder="Senha"
+      required="required"
+      name="password"
+      data-testid="password-input"
+      onChange={(e) => handleChange(e.target.value, setPassword, setPasswordValid)}
+      isValid={passwordValid}
+      isInvalid={!passwordValid}
+    />
+    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+    <Form.Control.Feedback type="invalid">
+      Your password must contain at least 6 characters
       </Form.Control.Feedback>
-    </Form.Group>
-  );
-};
+  </Form.Group>
+);
 
 PasswordField.propTypes = {
   setPassword: PropTypes.func.isRequired,
