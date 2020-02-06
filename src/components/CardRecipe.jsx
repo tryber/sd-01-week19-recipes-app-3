6dec18f101d8c6b17231cd7388183c200564f679
Henrique Eyer <henriqueeyer@outlook.com>
2020-02-06 17:13:06 -0300

    Terminando card

--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types'; 
 import { Link } from 'react-router-dom';
-import './CardRecipe.css'
+import './CardRecipe.css';
 
 const CardRecipe = ({
   img = 'https://cdn.britannica.com/41/177341-050-8688C353/Chelsea-buns.jpg',
   category = 'Dessert',
   title = 'Chelsea Buns',
-  id = '200',
+  id = 200,
   type = 'comida',
 }) => (
-    <div className="CardRecipe">
-      <Link className="LinkRecipe" to={`/receitas/${type}/${id}`}>
-        <img className="imgRecipe" src={img} alt='Foto Receita' />
-        <h4>{category}</h4>
-        <h2>{title}</h2>
-      </Link>
-    </div>
-  );
+  <div className="CardRecipe">
+    <Link className="LinkRecipe" to={`/receitas/${type}/${id}`}>
+      <img className="imgRecipe" src={img} alt='Foto Receita' />
+      <h4 className="categoryRecipe">{category}</h4>
+      <h2 className="titleRecipe">{title}</h2>
+    </Link>
+  </div>
+);
 
 export default CardRecipe;
+
+CardRecipe.propTypes = {
+  img: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+};
