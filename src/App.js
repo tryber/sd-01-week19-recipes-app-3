592230a107c9c0fb3905afef4c471d7d58d88704
Henrique Eyer <55408235+HenriqueEyer@users.noreply.github.com>
2020-02-07 15:28:24 -0300

    FIx CC

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Foods from './components/Foods';
 import Drinks from './components/Drinks';
-
-//import Home from './components/Home';
 // import CardRecipe from './components/CardRecipe';
-
-
 import Login from './components/Login';
-
 
 function App() {
   return (
