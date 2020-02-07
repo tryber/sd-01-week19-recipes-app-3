9821765a1cda926c015a7d4b2408b1c4f4a40dd8
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-07 19:34:46 -0200

    code Climate

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
 const SubmitButton = ({ emailValid, passwordValid }) => {
   const fieldsValid = emailValid && passwordValid;
@@ -13,7 +13,7 @@ const SubmitButton = ({ emailValid, passwordValid }) => {
       data-testid="login-submit-btn"
       disabled={!fieldsValid}
       variant={!fieldsValid ? 'outline-danger' : 'outline-success'}
-      onClick={() => history.push("/comidas")}
+      onClick={() => history.push('/comidas')}
     > Entrar </Button>
   );
 };
