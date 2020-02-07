240d702cb542b9cbe305b2e31d63586e7461f559
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-07 16:26:08 -0200

    Ok?

--- src/service/FetchingAPI.jsx
+++ src/service/FetchingAPI.jsx
@@ -3,12 +3,27 @@ export const getMeals = async (request) => {
   return response.json();
 };
 
-export const getIngredientImage = async (request) => {
+export const getIngredientMealsImage = async (request) => {
   const response = await fetch(`https://www.themealdb.com/images/ingredients/${request}`);
   return response;
 };
 
-export const getMealImage = async (request) => {
+export const getMealsImage = async (request) => {
   const response = await fetch(`https://www.themealdb.com/images/media/meals/${request}/preview`);
+  return response;
+};
+
+export const getDrinks = async (request) => {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/${request}`);
+  return response.json();
+};
+
+export const getIngredientDrinksImage = async (request) => {
+  const response = await fetch(`https://www.thecocktaildb.com/images/ingredients/${request}`);
+  return response;
+};
+
+export const getDrinksImage = async (request) => {
+  const response = await fetch(`https://www.thecocktaildb.com/images/media/drink/${request}/preview`);
   return response;
 };
