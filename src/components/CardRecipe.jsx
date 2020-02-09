ae646125f65fedbf02cf94203a4d498291066319
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-09 02:45:59 -0200

    Render Recipes

--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -4,20 +4,22 @@ import { Link } from 'react-router-dom';
 import './CardRecipe.css';
 
 const CardRecipe = ({
-  img = 'https://cdn.britannica.com/41/177341-050-8688C353/Chelsea-buns.jpg',
-  category = 'Dessert',
-  title = 'Chelsea Buns',
-  id = 200,
-  type = 'comida',
-}) => (
-  <div className="CardRecipe">
-    <Link className="LinkRecipe" to={`/receitas/${type}/${id}`}>
-      <img className="imgRecipe" src={img} alt="Foto Receita" />
-      <h4 className="categoryRecipe">{category}</h4>
-      <h2 className="titleRecipe">{title}</h2>
-    </Link>
-  </div>
-);
+  image,
+  category,
+  title,
+  id,
+  type,
+}) => {
+  return (
+    <div className="CardRecipe">
+      <Link className="LinkRecipe" to={`/receitas/${type}/${id}`}>
+        <img className="imgRecipe" src={image} alt="Foto Receita" data-testid={`${id}-card-img`} />
+        <h4 className="categoryRecipe" data-testid={`${id}-card-category`}>{category}</h4>
+        <h2 className="titleRecipe" data-testid={`${id}-card-name`} >{title}</h2>
+      </Link>
+    </div>
+  );
+}
 
 export default CardRecipe;
 
--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -4,6 +4,7 @@ import { getMeals } from '../service/FetchingAPI';
 import use12Recipes from '../hooks/use12Recipes';
 import Header from './Header';
 import { useLocation } from 'react-router-dom';
+import CardRecipe from './CardRecipe';
 
 const Foods = () => {
   const [url, setUrl] = useState('random.php');
@@ -15,7 +16,13 @@ const Foods = () => {
   return (
     <div>
       <Header location={location} />
-
+      {recipes && recipes.map(({ strMeal, strCategory, strMealThumb, idMeal }) => (<CardRecipe
+        title={strMeal}
+        category={strCategory}
+        image={strMealThumb}
+        id={idMeal}
+        type='comidas'
+      />))}
     </div>
   );
 }
