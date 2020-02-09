8c45ad3453c96680f4dea5abe67b414b77a08adf
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-09 19:40:26 -0200

    Ref

--- src/hooks/useUrlSearch.jsx
+++ src/hooks/useUrlSearch.jsx
@@ -2,12 +2,20 @@ import { useEffect, useState } from 'react';
 
 const settingUrl = (text, searchType, setUrl) => {
   const isTextEmpty = text !== '';
-  if (isTextEmpty && searchType === 'ingredient') {
-    setUrl(`filter.php?i=${text}`);
-  } else if (isTextEmpty && searchType === 'name') {
-    setUrl(`search.php?s=${text}`);
-  } else if (isTextEmpty && searchType === 'letter') {
-    setUrl(`search.php?f=${text}`);
+  if (isTextEmpty) {
+    switch (searchType) {
+      case 'ingredient':
+        setUrl(`filter.php?i=${text}`);
+        break;
+      case 'name':
+        setUrl(`search.php?s=${text}`);
+        break;
+      case 'letter':
+        setUrl(`search.php?f=${text}`);
+        break;
+      default:
+        break;
+    }
   } else {
     setUrl('random.php');
   }
