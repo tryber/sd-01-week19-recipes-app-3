934ccc3dd894ba98d70eb6c4af8449fabcea8a43
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-08 22:45:02 -0200

    Fetching 12 recipes

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getMeals } from '../service/FetchingAPI';
 
 export default function Foods() {
+
+  getMeals('random.php')
+
+  useEffect(() => {
+    const array = [];
+    for (let i = 0; i < 12; i += 1) {
+      getMeals('random.php')
+      .then(
+        (resolve) => {
+          if(array.includes(resolve.meals[0])) {
+            i -= 1
+          } else {
+            array.push(resolve.meals[0])
+          }
+        },
+      )
+    }
+
+    console.log(array)
+    
+  }, []);
   return (
     <div>
       <h1> Comida </h1>
