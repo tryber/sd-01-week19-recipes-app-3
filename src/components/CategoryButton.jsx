9e4b1586b5c5521a6cfbd3b2cbb7344230c0445e
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-09 19:57:49 -0200

    Category Button

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CategoryButton = ({title, changeCategory}) => (
+const CategoryButton = ({ title, changeCategory }) => (
   <div className="ButtonShare">
     <button
-    type="button"
-    value={title}
-    onClick={(e) => changeCategory(e.target.value)}
-    data-testid={`${title}-category-filter`}
+      type="button"
+      value={title}
+      onClick={(e) => changeCategory(e.target.value)}
+      data-testid={`${title}-category-filter`}
     >{title}</button>
   </div>
 );
+
+CategoryButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  changeCategory: PropTypes.func.isRequired,
+};
 
 export default CategoryButton;
