ff9984def0c4013bedb2e51c9ce2728a913c48f6
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-09 22:53:03 -0200

    Fixing Categories

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getDrinks } from '../service/FetchingAPI';
+import { ReciperContext } from '../context/ReciperContext';
+import useUrlSearch from '../hooks/useUrlSearch';
+import useCategories from '../hooks/useCategories';
+import useRecipesSrcBarFil from '../hooks/useRecipesSrcBarFil';
+import useRecipesCtgFil from '../hooks/useRecipesCtgFil';
+import Header from './Header';
+import CardRecipe from './CardRecipe';
+import CategoryButton from './CategoryButton';
+import LowerMenu from './LowerMenu';
 
-export default function Drinks() {
+const Drinks = () => {
+  const location = useLocation();
+  const pathname = location.pathname.slice(1);
+
+  const { search: { text, typeSearch } } = useContext(ReciperContext);
+
+  const [category, setCategory] = useState('All');
+
+  const categories = useCategories(getDrinks, pathname);
+  const url = useUrlSearch(text, typeSearch);
+  const { recipes } = useRecipesSrcBarFil(getDrinks, url, pathname);
+  const data = useRecipesCtgFil(recipes, category, getDrinks, text, pathname);
+
   return (
     <div>
-      <h1> Bebida </h1>
+      <Header location={location} />
+      <CategoryButton key="All" title="All" changeCategory={setCategory} />
+      {categories && categories.map(({ strCategory }) =>
+        <CategoryButton key={strCategory} title={strCategory} changeCategory={setCategory} />)}
+      {data && data.map(({ strDrink, strCategory, strDrinkThumb, idDrink }) =>
+        (<CardRecipe
+          title={strDrink}
+          category={strCategory}
+          image={strDrinkThumb}
+          id={idDrink}
+          type={pathname}
+          key={idDrink}
+        />))}
+      <LowerMenu />
     </div>
   );
-}
+};
+
+export default Drinks;
--- src/hooks/useCategories.jsx
+++ src/hooks/useCategories.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
 
-const fetch5Categories = async (fetchingAPI, url, setCategories) => {
+const fetch5Categories = async (fetchingAPI, url, setCategories, node) => {
   const categories = [];
-  await fetchingAPI(url).then((resolve) => categories.push(...resolve.categories));
+  await fetchingAPI(url).then((resolve) => categories.push(...resolve[node]));
   setCategories(categories.slice(0, 5));
 };
 
-const useCategories = (fetchingAPI) => {
+const useCategories = (fetchingAPI, pathname) => {
   const [categories, setCategories] = useState([]);
+  const node = pathname === 'comidas' ? 'meals' : 'drinks';
 
   useEffect(() => {
-    fetch5Categories(fetchingAPI, 'categories.php', setCategories);
+    fetch5Categories(fetchingAPI, 'list.php?c=list', setCategories, node);
   }, []);
 
   return categories;
