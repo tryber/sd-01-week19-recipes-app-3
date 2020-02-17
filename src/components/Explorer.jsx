9df96c7f4f89f42d946e2888a419dbcb0e1115b1
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-17 00:24:30 -0300

    Route

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { getMeals, getDrinks } from '../service/FetchingAPI';
+
+const fetchId = async (isFoodOrDrink, setId) => {
+  if (isFoodOrDrink === 'comidas') {
+    const id = await getMeals('random.php');
+    setId(id.meals[0].idMeal);
+  } else {
+    const id = await getDrinks('random.php');
+    setId(id.drinks[0].idDrink);
+  }
+};
 
 const Explorer = ({ match: { params: { isFoodOrDrink: isFoodOrDrink } } }) => {
+  const [id, setId] = useState();
   const history = useHistory();
+
+  useEffect(() => {
+    fetchId(isFoodOrDrink, setId)
+  }, []);
+console.log(id)
   return (
     <div>
       <h1>{isFoodOrDrink}</h1>
       <button data-testid="explore-by-ingredient" onClick={() => history.push(`/explorar/${isFoodOrDrink}/ingredientes`)}>Por ingredientes</button>
-      { isFoodOrDrink === 'comidas' && <button data-testid="explore-by-area" onClick={() => history.push('/explorar/comidas/area')} >Por local de origem</button>}
-      <button data-testid="explore-surprise" >Me surpreenda!</button>
+      {isFoodOrDrink === 'comidas' && <button data-testid="explore-by-area" onClick={() => history.push('/explorar/comidas/area')} >Por local de origem</button>}
+      <button data-testid="explore-surprise" onClick={() => history.push(`/explorar/${isFoodOrDrink}/${id}`)}>Me surpreenda!</button>
     </div>
   );
 };
