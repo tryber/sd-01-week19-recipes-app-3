073ac4323a91e040cf8c2ea180de058eca9ba59a
henriqueeyer <henriqueeyer@outlook.com>
2020-02-27 15:23:01 -0300

    aa

---
+++ src/components/Recommendation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Recommendation = ({
+  match: { params: { foodordrink } } },
+  ingredient) => (
+    <div className="Recommendation">
+
+    </div>
+  );
+
+export default Recommendation;
